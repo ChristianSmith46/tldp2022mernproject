556a5e9f29a00e348f7da18028f6bb22357703ee
ChristianSmith46 <christian.46.smith@gmail.com>
2024-09-12 15:23:02 -0400

    Use hashing on passwords being stored

--- server_nodejs/controllers/account-controller.js
+++ server_nodejs/controllers/account-controller.js
@@ -1,26 +1,32 @@
 import Customer from '../models/customer.js'
+import bcrypt from 'bcrypt';
 
 
 const accountController = {
 
   login: function(req, res) {
     Customer.findOne(
-      {CUSTOMER_NAME: req.body.username, PASSWORD: req.body.password}, 
+      {CUSTOMER_NAME: req.body.username}, 
       '', 
       function(err, customer){
         if(err || !customer) {
           res.sendStatus(401) // Unauthorized
         } else {
-          res.sendStatus(200) // OK
+          const compare = bcrypt.compareSync(req.body.password, customer.PASSWORD);
+          if (compare) return res.sendStatus(200) // OK
+          return res.sendStatus(401);
         }
       }
     )
   },
 
   register: function(req, res) {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPass = bcrypt.hashSync(req.body.password, salt)
     Customer.create({ 
       CUSTOMER_NAME: req.body.name, 
-      PASSWORD: req.body.password,
+      PASSWORD: hashedPass,
       EMAIL: req.body.email
     }).then(
       () => res.sendStatus(201), // OK
